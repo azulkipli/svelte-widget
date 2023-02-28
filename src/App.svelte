f80ec4f1e2c56ae85378bbfb4de59670b142a4c5
azulkipli <azulkipli@gmail.com>
2023-02-28 10:47:31 +0700

    unused svelte-tel-input

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,7 @@
   import Counter from "./components/Counter.svelte";
   import Info from "./components/Info.svelte";
   import Product from "./components/Product.svelte";
-  import InputPhone from "./components/InputPhone.svelte";
-  import InputPhoneFlag from "./components/InputPhoneFlag.svelte";
+  // import InputPhone from "./components/InputPhone.svelte";
   // import { onMount } from "svelte";
   import Icon from "@iconify/svelte";
   import Lazy from 'svelte-lazy';
@@ -45,8 +44,6 @@
 
   {#if display === "thumbnail"}
    <div class="wrapper">
-    <InputPhone />
-    <InputPhoneFlag />
     <Product {text} />
    </div>
   {/if}
